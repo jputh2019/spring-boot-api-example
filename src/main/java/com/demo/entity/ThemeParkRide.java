192fd85514a65bcb4ed53656372bd6453f13c8a1
julie <jjputh@gmail.com>
2020-07-28 18:02:57 -0400

    initial commit

---
+++ src/main/java/com/demo/entity/ThemeParkRide.java
@@ -0,0 +1,77 @@
+package com.demo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+public class ThemeParkRide {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  @NotEmpty
+  private String name;
+  @NotEmpty
+  private String description;
+  private int thrillFactor;
+  private int vomitFactor;
+  
+  public ThemeParkRide() {}
+
+  public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getThrillFactor() {
+		return thrillFactor;
+	}
+
+	public void setThrillFactor(int thrillFactor) {
+		this.thrillFactor = thrillFactor;
+	}
+
+	public int getVomitFactor() {
+		return vomitFactor;
+	}
+
+	public void setVomitFactor(int vomitFactor) {
+		this.vomitFactor = vomitFactor;
+	}
+  
+  public ThemeParkRide(String name, String description, int thrillFactor, int vomitFactor) {
+    this.name = name;
+    this.description = description;
+    this.thrillFactor = thrillFactor;
+    this.vomitFactor = vomitFactor;
+  }
+
+}
---
+++ src/main/java/com/demo/repository/ThemeParkRideRepository.java
@@ -0,0 +1,13 @@
+package com.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.entity.ThemeParkRide;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
